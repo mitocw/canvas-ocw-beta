b1b13ada97b140227a53b58cd0b3fc0830b4448c
Jean-Michel Claus <jmc@math.mit.edu>
2021-03-12 11:28:15 +0100

    Change app title to 'OCW Canvas Intelligence'. Add header.

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h2 className="header__text-1"><span className="header__text-2">OCW </span>Canvas Intelligence</h2>
+    </header>
+  );
+}
