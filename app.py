eb60d6b626acf1aefd4de4c98ed159bc04259e95
Jean-Michel Claus <jmc@math.mit.edu>
2021-03-30 00:51:56 +0200

    Add Whoosh and return search results with its index

--- app.py
+++ app.py
@@ -1,14 +1,75 @@
 import os
 import requests
+import json
 from flask import current_app, Flask, jsonify, make_response, redirect, render_template, request
 from jinja2 import Template
 from string import Template as Temp
 from python_graphql_client import GraphqlClient
-
+from datetime import datetime
+from whoosh.fields import Schema, ID, TEXT, NUMERIC, DATETIME
+from whoosh import index, qparser
 
 app = Flask(__name__, static_folder='./client/build', static_url_path='/')
 app.config.from_object('config')
 
+if not os.path.exists("indexdir"):
+    schema = Schema(
+        id=ID(stored=True),
+        name=TEXT(stored=True),
+        course_code=TEXT(stored=True),
+        dept=TEXT(stored=True),
+        created_at=DATETIME(stored=True),
+        updated_at=DATETIME(stored=True),
+        n_modules=NUMERIC(stored=True),
+        n_assignments=NUMERIC(stored=True),
+        n_pages=NUMERIC(stored=True),
+        n_files=NUMERIC(stored=True),
+        n_quizzes=NUMERIC(stored=True),
+        n_visible_tabs=NUMERIC(stored=True),
+        n_students=NUMERIC(stored=True)
+    )
+    os.mkdir("indexdir")
+    ix = index.create_in('indexdir', schema)
+    writer = ix.writer()
+    with open(r'coursewares.json') as json_file:
+        data = json.load(json_file)
+    for id in data:
+        courseware = data[id]
+        writer.add_document(
+            id=id,
+            name=courseware['name'],
+            course_code=courseware['course_code'],
+            dept=courseware['dept'],
+            created_at=datetime.fromisoformat(courseware['created_at'][:-1]),
+            updated_at=datetime.fromisoformat(courseware['updated_at'][:-1]),
+            n_modules=courseware['n_modules'],
+            n_assignments=courseware['n_assignments'],
+            n_pages=courseware['n_pages'],
+            n_files=courseware['n_files'],
+            n_quizzes=courseware['n_quizzes'],
+            n_visible_tabs=courseware['n_visible_tabs'],
+            n_students=courseware['n_students']
+        )
+    writer.commit()
+
+ix = index.open_dir('indexdir')
+qp = qparser.MultifieldParser(['name', 'dept'], ix.schema)
+
+# Example of one of the courseware of our json file:
+# "3196": {
+#     "name": "10.34 Numerical Methods: Chem Eng",
+#     "course_code": "10.34",
+#     "dept": "10 - Department of Chemical Engineering",
+#     "created_at": "2020-05-28T02:21:34Z",
+#     "updated_at": "2020-12-23T17:20:26Z",
+#     "n_modules": 17,
+#     "n_assignments": 45,
+#     "n_pages": 17,
+#     "n_files": 294,
+#     "n_quizzes": 2,
+#     "n_visible_tabs": 9,
+#     "n_students": 69
+# },
 
 def error(exception=None):
     """ render error page
@@ -26,9 +87,21 @@ def index():
     index_template = Template(open('./client/build/index.html').read())
     return index_template.render(content_item_return_url='')
 
-
 @app.route('/search', methods=['POST'])
 def search():
+    if request.method == 'POST':
+        search_term = request.form['query']
+        q = qp.parse(search_term)
+        coursewares = []
+        with ix.searcher() as searcher:
+            results = searcher.search(q)
+            for r in results:
+                coursewares.append(r.fields())
+
+    return jsonify(coursewares)
+
+@app.route('/search_contentful', methods=['POST'])
+def search_contenful():
     result = []
     if request.method == 'POST':
         # Instantiate graphql client with Contentful's endpoint
