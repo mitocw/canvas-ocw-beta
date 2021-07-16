876109ed43d75871ff0820c88b9974490a3aa5dd
Jean-Michel Claus <jmc@math.mit.edu>
2021-07-16 18:34:36 +0200

    Implement Google session-based authentication

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 import requests
 import json
-from flask import current_app, Flask, jsonify, make_response, redirect, render_template, request
+import functools
 import gspread
+import google.oauth2.credentials
+import googleapiclient.discovery
+from flask import current_app, Flask, jsonify, make_response, redirect, render_template, request, session
 from jinja2 import Template
 from string import Template as Temp
 from oauth2client.service_account import ServiceAccountCredentials
+from authlib.integrations.requests_client import OAuth2Session
 from python_graphql_client import GraphqlClient
 from datetime import datetime
 from whoosh.fields import Schema, ID, TEXT, NUMERIC, DATETIME
@@ -90,11 +94,23 @@ qp = qparser.MultifieldParser(['name', 'course_code'], ix.schema)
 
 # Google sheets authorization
 scope = ['https://www.googleapis.com/auth/drive']
-# creds = ServiceAccountCredentials.from_json_keyfile_name(current_app.config['GSCREDS'], scope)
 creds = ServiceAccountCredentials.from_json_keyfile_name('./gsheets_credentials.json', scope)
 gsclient = gspread.authorize(creds)
 spreadsheet = gsclient.open('publication_candidate_notes')
 worksheet = spreadsheet.worksheet('Sheet1')
+
+# Google authentication
+ACCESS_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent'
+AUTHORIZATION_SCOPE ='openid email profile'
+
+AUTH_REDIRECT_URI = app.config['GOOGLE_AUTH_REDIRECT_URI']
+BASE_URI = app.config['GOOGLE_BASE_URI']
+CLIENT_ID = app.config['GOOGLE_CLIENT_ID']
+CLIENT_SECRET = app.config['GOOGLE_CLIENT_SECRET']
+
+AUTH_TOKEN_KEY = 'auth_token'
+AUTH_STATE_KEY = 'auth_state'
 
 def paginate(data, offset=0, limit=5):
     return data[offset: offset + limit]
@@ -106,13 +122,103 @@ def error(exception=None):
     """
     return render_template('error.html')
 
+def is_logged_in():
+    return True if AUTH_TOKEN_KEY in session else False
+
+def build_credentials():
+    if not is_logged_in():
+        raise Exception('User must be logged in')
+
+    oauth2_tokens = session[AUTH_TOKEN_KEY]
+    
+    return google.oauth2.credentials.Credentials(
+        oauth2_tokens['access_token'],
+        refresh_token=oauth2_tokens['refresh_token'],
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        token_uri=ACCESS_TOKEN_URI
+    )
+
+def get_user_info():
+    credentials = build_credentials()
+
+    oauth2_client = googleapiclient.discovery.build(
+        'oauth2', 'v2',
+        credentials=credentials
+    )
+
+    return oauth2_client.userinfo().get().execute()
+
+def no_cache(view):
+    @functools.wraps(view)
+    def no_cache_impl(*args, **kwargs):
+        response = make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return functools.update_wrapper(no_cache_impl, view)
+
+@app.route('/google/login')
+@no_cache
+def login():
+    oauth2_session = OAuth2Session(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        scope=AUTHORIZATION_SCOPE,
+        redirect_uri=AUTH_REDIRECT_URI
+    )
+  
+    uri, state = oauth2_session.create_authorization_url(AUTHORIZATION_URL)
+
+    session[AUTH_STATE_KEY] = state
+    session.permanent = True
+
+    return redirect(uri, code=302)
+
+@app.route('/google/auth')
+@no_cache
+def auth_redirect():
+    req_state = request.args.get('state', default=None, type=None)
+
+    if req_state != session[AUTH_STATE_KEY]:
+        response = make_response('Invalid state parameter', 401)
+        return response
+    
+    oauth2_session = OAuth2Session(
+        CLIENT_ID,
+        CLIENT_SECRET,
+        scope=AUTHORIZATION_SCOPE,
+        state=session[AUTH_STATE_KEY],
+        redirect_uri=AUTH_REDIRECT_URI
+    )
+
+    oauth2_tokens = oauth2_session.fetch_token(
+        ACCESS_TOKEN_URI,
+        authorization_response=request.url
+    )
+
+    session[AUTH_TOKEN_KEY] = oauth2_tokens
+
+    return redirect(BASE_URI, code=302)
+
+@app.route('/google/logout')
+@no_cache
+def logout():
+    session.pop(AUTH_TOKEN_KEY, None)
+    session.pop(AUTH_STATE_KEY, None)
+
+    return redirect(BASE_URI, code=302)
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET'])
 def index():
-    # React client build entry point. It contains a "content_item_return_url" placeholder that is loaded in
-    # a JS var as it is needed in the reuse form located in client/components/VideoCard.js
-    index_template = Template(open('./client/build/index.html').read())
-    return index_template.render(content_item_return_url='')
+    if is_logged_in():
+        # React client build entry point.
+        index_template = Template(open('./client/build/index.html').read())
+        return index_template.render()
+    return '<a class="button" href="/google/login">Google Login</a>'
 
 @app.route('/search', methods=['POST'])
 def search():
