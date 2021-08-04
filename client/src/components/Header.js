2fc8aea160b74f221f8969905a83261bc4afebe6
Jean-Michel Claus <jmc@math.mit.edu>
2021-08-04 16:06:27 +0200

    Change app title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.scss';
 export default function Header() {
   return (
     <header className="header">
-      <h2 className="header__text-1"><span className="header__text-2">OCW </span>Canvas Intelligence</h2>
+      <h2 className="header__text-1"><span className="header__text-2">OCW Team </span>Canvas Intelligence</h2>
       <a className="header__text-3" href="/google/logout">Log out</a>
     </header>
   );
